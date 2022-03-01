ca8ff61f5a652943b1bd68b0d62ef4a3df145d7d
nazu280 <66703146+nazu280@users.noreply.github.com>
2022-03-01 19:39:20 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+var typingBool = false; 
+var typingIdx=0; 
+var liIndex = 0;
+var liLength = $(".typing-txt>ul>li").length;
+var del = -1;
+var repeatInt= null;
+var tyInt = null;
+
+
+var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+
+typingTxt=typingTxt.split("");
+
+if(typingBool==false){ 
+    typingBool=true; 
+    tyInt = setInterval(typing,200);
+} 
+     
+function typing(){ 
+  if(typingIdx<typingTxt.length){ 
+   $(".typing").append(typingTxt[typingIdx]); 
+     typingIdx++; 
+    if(typingIdx == typingTxt.length){
+        clearInterval(tyInt);
+         setTimeout(function(){
+           tyInt = setInterval(typing,200);
+         },1000);
+       }
+   } else{ 
+       if(-typingTxt.length-1 < del ){
+          $(".typing").html(typingTxt.slice(0, del))
+          del--;
+       }else{
+         if(liIndex >= liLength-1){
+              liIndex=0;
+         }else{
+           liIndex++;
+         typingIdx=0;
+         del= -1;
+         typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+         clearInterval(tyInt);
+         setTimeout(function(){
+           tyInt = setInterval(typing,200);
+         },1000);
+       }
+     
+
+    } 
+}  
